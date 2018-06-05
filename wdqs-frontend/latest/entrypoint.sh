8e2da5f188419cc5f5e9399dcf4480bc4b7e5fc9
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-06-05 18:00:24 +0200

    Simplify and unify unset variable tests in entrypoint.sh
    Several entrypoint.sh scripts check if all required variables are set,
    and some had slightly different code. Unify all this and simplify it a
    bit as well:
    
    - Inline the array of required variables, since it’s only used once.
    - Consistently check whether the variable is unset or empty.
    - Never use eval.
    
    Also adds trailing newlines where they were missing.

--- wdqs-frontend/latest/entrypoint.sh
+++ wdqs-frontend/latest/entrypoint.sh
@@ -2,15 +2,14 @@
 # This file is provided by the wikibase/wdqs-frontend docker image.
 
 # Test if required environment variables have been set
-if [ -z "$WIKIBASE_HOST" ]; then
-echo "WIKIBASE_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-exit 1;
-fi
-
-if [ -z "$WDQS_HOST" ]; then
-echo "WDQS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-exit 1;
-fi
+for requiredVariable in WIKIBASE_HOST WDQS_HOST; do
+    if [[ ! -v $requiredVariable || -z ${!requiredVariable} ]]; then
+        printf >&2 '%s is required, but either unset or empty. You should pass it to docker. See: %s\n' \
+            "$requiredVariable" \
+            'https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file'
+        exit 1
+    fi
+done
 
 set -eu
 
--- wdqs-proxy/latest/entrypoint.sh
+++ wdqs-proxy/latest/entrypoint.sh
@@ -2,10 +2,14 @@
 # This file is provided by the wikibase/wdqs-proxy docker image.
 
 # Test if required environment variables have been set
-if [ -z "$PROXY_PASS_HOST" ]; then
-echo "PROXY_PASS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-exit 1;
-fi
+for requiredVariable in PROXY_PASS_HOST; do
+    if [[ ! -v $requiredVariable || -z ${!requiredVariable} ]]; then
+        printf >&2 '%s is required, but either unset or empty. You should pass it to docker. See: %s\n' \
+            "$requiredVariable" \
+            'https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file'
+        exit 1
+    fi
+done
 
 set -eu
 
